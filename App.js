ce35b340f4bac3e42ac329ebb2a78a01d96a961e
Gautham-25 <158817524+Gautham-25@users.noreply.github.com>
2024-02-16 22:50:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,1 @@
-import React, { Component } from 'react';
-function App(){
-  return(
-    <>
-    <h1>Welcome</h1>
-    <h2>Addition of two numbers is {2+2} </h2>
-    </>
-  )
 
-}
-export default App
